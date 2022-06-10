16a10dbe8a26365f8c9abdb87465490f898bd4e1
charlielao <43299848+charlielao@users.noreply.github.com>
2022-06-10 14:07:56 +0100

    unknown parameter case

--- 6.June meeting/codegpflow.py
+++ 6.June meeting/codegpflow.py
@@ -246,12 +246,14 @@ energy_kernel.RBFa.variance = gpflow.Parameter(energy_kernel.RBFa.variance.numpy
 energy_kernel.RBFv.variance = gpflow.Parameter(energy_kernel.RBFv.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
 energy_kernel.RBFa.lengthscales = gpflow.Parameter(energy_kernel.RBFa.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
 energy_kernel.RBFv.lengthscales = gpflow.Parameter(energy_kernel.RBFv.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
-# %% prior
+# %% 
+# prior
 pred = tf.zeros((test_points.shape[0], 1), dtype=tf.float64)
 var = tf.linalg.diag_part(energy_kernel(test_points))
 plotting(pred, var[:int(var.shape[0]/2)], eval_points=(test_xx, test_vv), data=(X,Y),save=0, name="", angle1=10, angle2=-65, acc=1, lml=0)
 plotting(pred, var[int(var.shape[0]/2):], eval_points=(test_xx, test_vv), data=(X,Y),save=0, name="", angle1=10, angle2=-65, acc=0, lml=0)
 # %%
+# posterior
 m = gpflow.models.GPR(data=(X, tf.reshape(tf.transpose(tf.concat([Y[:,1,None],Y[:,0,None]],1)),(Y.shape[0]*2,1))), kernel=energy_kernel, mean_function=Zero_mean(output_dim=2))
 
 opt = gpflow.optimizers.Scipy()
@@ -298,5 +300,125 @@ def plotting_samples(kernel, n_of_samples, eval_points, acc):
 
 # %%
 plotting_samples(moi, 1, tf.meshgrid(tf.linspace(-test_range,test_range,20),tf.linspace(-test_range,test_range,20)), 1)
+
+# %%
+class SHO_Energy_Invariance_unknown_parameter(gpflow.kernels.Kernel):
+    def __init__(self, invariance_range, invar_density):
+        super().__init__(active_dims=[0, 1])
+        self.k = gpflow.Parameter(3., transform =tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.)))
+        self.m = gpflow.Parameter(1., transform =tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.)))
+        self.jitter = gpflow.kernels.White(1e-5)
+        self.RBFa = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.RBFv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.Ka =  self.RBFa + self.jitter
+        self.Kv =  self.RBFv + self.jitter
+        invariance_xs = tf.linspace(-invariance_range,invariance_range,invar_density)
+        invariance_vs = tf.linspace(-invariance_range,invariance_range,invar_density)
+        invariance_xx, invariance_vv = tf.meshgrid(invariance_xs, invariance_vs)
+        self.invar_grids = tf.stack([tf.reshape(invariance_xx,[-1]), tf.reshape(invariance_vv,[-1])], axis=1)
+
+    def K(self, X, X2=None):
+        if X2 is None:
+            X2 = X
+        n = X.shape[0]
+        m = X2.shape[0]
+
+        zeros_nn =tf.zeros((n,n), dtype=tf.float64)
+        zeros_nm =tf.zeros((n,m), dtype=tf.float64)
+        zeros_mm =tf.zeros((m,m), dtype=tf.float64)
+        
+        Ka_X1X1  = self.Ka(X) 
+        Kv_X1X1  = self.Kv(X) 
+        K_X1X1   = tf.concat([tf.concat([Ka_X1X1,zeros_nn],1),tf.concat([zeros_nn,Kv_X1X1],1)],0)
+
+        Ka_X1X2  = self.Ka(X, X2) 
+        Kv_X1X2  = self.Kv(X, X2) 
+        K_X1X2   = tf.concat([tf.concat([Ka_X1X2,zeros_nm],1),tf.concat([zeros_nm,Kv_X1X2],1)],0)
+        
+        K_X2X1   = tf.transpose(K_X1X2)
+        
+        Ka_X2X2  = self.Ka(X2) 
+        Kv_X2X2  = self.Kv(X2) 
+        K_X2X2   = tf.concat([tf.concat([Ka_X2X2,zeros_mm],1),tf.concat([zeros_mm,Kv_X2X2],1)],0)
+        
+        Ka_X1Xg  = self.Ka(X, self.invar_grids) 
+        Kv_X1Xg  = self.Kv(X, self.invar_grids) 
+        K_X1Xg = tf.concat([Ka_X1Xg, Kv_X1Xg],0)
+
+        Ka_X2Xg =  self.Ka(X2, self.invar_grids) 
+        Kv_X2Xg =  self.Kv(X2, self.invar_grids)
+        K_X2Xg = tf.concat([Ka_X2Xg, Kv_X2Xg],0)
+
+        Ka_XgXg = self.Ka(self.invar_grids) 
+        Kv_XgXg = self.Kv(self.invar_grids) 
+        
+        x_g_1 = tf.ones([n, 1], dtype=tf.float64) * self.invar_grids[:,0] * self.k
+        x_g_dot_1 = tf.ones([n, 1], dtype=tf.float64) * self.invar_grids[:,1] * self.m
+        x_g_1_stacked = tf.concat([x_g_dot_1, x_g_1],0) 
+        
+        x_g_2 = tf.ones([m, 1], dtype=tf.float64) * self.invar_grids[:,0] * self.k
+        x_g_dot_2 = tf.ones([m, 1], dtype=tf.float64) * self.invar_grids[:,1] * self.m
+        x_g_2_stacked = tf.concat([x_g_dot_2, x_g_2],0) 
+
+        x_g_squared = tf.tensordot(self.invar_grids[:,0,None],self.invar_grids[None,:,0],1) * self.k**2
+        x_g_dot_squared = tf.tensordot(self.invar_grids[:,1,None],self.invar_grids[None,:,1],1) * self.m**2
+        
+        A = tf.concat([tf.concat([K_X1X1, K_X1X2],1),tf.concat([K_X2X1, K_X2X2],1)],0) 
+        B1 = tf.multiply(K_X1Xg, x_g_1_stacked)
+        B2 = tf.multiply(K_X2Xg, x_g_2_stacked)
+        B = tf.concat([B1, B2], 0)
+        C = tf.transpose(B)
+        D = tf.multiply(x_g_dot_squared, Ka_XgXg) + tf.multiply(x_g_squared, Kv_XgXg) 
+        
+        return (A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D), 1), C, 1))[:2*n, 2*n:]
+
+    def K_diag(self, X):
+        n = X.shape[0]
+        zeros_nn =tf.zeros((n,n), dtype=tf.float64)
+        
+        Ka_X  = self.Ka(X) 
+        Kv_X  = self.Kv(X) 
+        K_X   = tf.concat([tf.concat([Ka_X,zeros_nn],1),tf.concat([zeros_nn,Kv_X],1)],0)
+        
+        Ka_Xg  = self.Ka(X, self.invar_grids) 
+        Kv_Xg  = self.Kv(X, self.invar_grids) 
+        K_Xg = tf.concat([Ka_Xg, Kv_Xg],0)
+
+        Ka_XgXg = self.Ka(self.invar_grids) 
+        Kv_XgXg = self.Kv(self.invar_grids) 
+        
+        x_g = tf.ones([n, 1], dtype=tf.float64) * self.invar_grids[:,0] * self.k
+        x_g_dot = tf.ones([n, 1], dtype=tf.float64) * self.invar_grids[:,1] * self.m
+        x_g_stacked = tf.concat([x_g_dot, x_g],0)
+        
+        x_g_squared = tf.tensordot(self.invar_grids[:,0,None],self.invar_grids[None,:,0],1) * self.k**2
+        x_g_dot_squared = tf.tensordot(self.invar_grids[:,1,None],self.invar_grids[None,:,1],1) * self.m**2
+        
+        A = K_X
+        B = tf.multiply(K_Xg, x_g_stacked)
+        C = tf.transpose(B)
+        D = tf.multiply(x_g_dot_squared, Ka_XgXg) + tf.multiply(x_g_squared, Kv_XgXg)
+        
+        return tf.linalg.tensor_diag_part(A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D),1), C, 1))
+
+# %%
+energy_kernel_unknown = SHO_Energy_Invariance_unknown_parameter(5, 20)
+set_trainable(energy_kernel_unknown.jitter.variance, False)
+energy_kernel_unknown.RBFa.variance = gpflow.Parameter(energy_kernel_unknown.RBFa.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
+energy_kernel_unknown.RBFv.variance = gpflow.Parameter(energy_kernel_unknown.RBFv.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
+energy_kernel_unknown.RBFa.lengthscales = gpflow.Parameter(energy_kernel_unknown.RBFa.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
+energy_kernel_unknown.RBFv.lengthscales = gpflow.Parameter(energy_kernel_unknown.RBFv.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(5.))) 
+# %%
+m = gpflow.models.GPR(data=(X, tf.reshape(tf.transpose(tf.concat([Y[:,1,None],Y[:,0,None]],1)),(Y.shape[0]*2,1))), kernel=energy_kernel_unknown, mean_function=Zero_mean(output_dim=2))
+
+opt = gpflow.optimizers.Scipy()
+opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(maxiter=100))
+pred, var = m.predict_f(test_points)
+print_summary(m)
+print(m.log_marginal_likelihood().numpy())
+print(degree_of_freedom(m, energy_kernel_unknown, X).numpy())
+# %%
+plotting(pred[:int(pred.shape[0]/2),:], var[:int(var.shape[0]/2),:], eval_points=(test_xx, test_vv), data=(X,Y),save=0, name="", angle1=10, angle2=-65, acc=1, lml=m.log_marginal_likelihood().numpy())
+plotting(pred[int(pred.shape[0]/2):,:], var[int(var.shape[0]/2):,:], eval_points=(test_xx, test_vv), data=(X,Y),save=0, name="", angle1=10, angle2=-65, acc=0, lml=m.log_marginal_likelihood().numpy())
 
 # %%
