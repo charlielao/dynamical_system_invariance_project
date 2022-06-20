6e4f14f2c4a97698528ca51286b364d388b30cb3
charlielao <43299848+charlielao@users.noreply.github.com>
2022-06-20 21:36:14 +0100

    tidy up

--- codes/damped.py
+++ codes/damped.py
@@ -6,26 +6,26 @@ from scipy.integrate import solve_ivp, odeint
 from gpflow.utilities import print_summary, positive, to_default_float, set_trainable
 import invariance_functions as inv
 
-test_grids = inv.get_test_points()
-print("SHM")
+test_grids = inv.get_grid_of_points(3, 40)
+print("SHM")  #switch
 for gamma in [0.01, 0.05, 0.1]:
     print("current damping: %s" %gamma)
-    data = inv.get_damped_SHM_data(gamma)
+    data = inv.get_damped_SHM_data(gamma, 30, 0.1) #switch
     for fixed in [True, False]:
         if fixed:
             fixed_mean = "fixed mean"
         else:
-            fixed_mean = "damping mean"
+            fixed_mean = "analytical damping mean"
         print(fixed_mean)
         for jitter in [1e-5, 1e-6]:
             print("current jitter %s" %jitter)
-            print("no invariance has log marginal likelihood %s" %inv.get_GPR_model(inv.get_MOI(jitter), inv.zero_mean(2), data, test_grids)[0].log_marginal_likelihood().numpy())
+            print("Naive GP lml: %s" %inv.get_GPR_model(inv.get_MOI(), inv.zero_mean(2), data, test_grids)[0].log_marginal_likelihood().numpy())
             for invar_density in [20]:
                     try:
-                        kernel = inv.get_SHM_Invariance(3, invar_density, jitter)
-                        mean_function = inv.damping_SHM_mean(kernel, fixed, gamma, mass=1)
+                        kernel = inv.get_SHM_Invariance(3, invar_density, jitter)#switch
+                        mean_function = inv.damping_SHM_mean(kernel, fixed, gamma, mass=1)#switch
                         m, pred, var = inv.get_GPR_model(kernel, mean_function, data, test_grids)
-                        print("density %s has %s log marginal likelihood"%(invar_density, m.log_marginal_likelihood().numpy()))
+                        print("Invariance GP density %s lml: %s" %(invar_density, m.log_marginal_likelihood().numpy()))
 
                     except tf.errors.InvalidArgumentError:
                         print("jitter too small")
--- codes/invariance_functions.py
+++ codes/invariance_functions.py
@@ -4,14 +4,12 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from scipy.integrate import solve_ivp, odeint
 from gpflow.utilities import print_summary, positive, to_default_float, set_trainable
+
 class MOI(gpflow.kernels.Kernel):
-    def __init__(self, jitter_size):
+    def __init__(self):
         super().__init__(active_dims=[0,1])
-        self.jitter = gpflow.kernels.White(jitter_size)
-        self.RBFa = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.RBFv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.Ka =  self.RBFa + self.jitter
-        self.Kv =  self.RBFv + self.jitter
+        self.Ka =  gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.Kv =  gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
     def K(self, X, X2=None):
         if X2 is None:
             X2 = X
@@ -39,11 +37,9 @@ class MOI(gpflow.kernels.Kernel):
 class SHM_Invariance(gpflow.kernels.Kernel):
     def __init__(self, invariance_range, invar_density, jitter_size):
         super().__init__(active_dims=[0, 1])
-        self.jitter = gpflow.kernels.White(jitter_size)
-        self.RBFa = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.RBFv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.Ka =  self.RBFa + self.jitter
-        self.Kv =  self.RBFv + self.jitter
+        self.Ka = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.Kv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.jitter = jitter_size
         invariance_xs = tf.linspace(-invariance_range,invariance_range,invar_density)
         invariance_vs = tf.linspace(-invariance_range,invariance_range,invar_density)
         invariance_xx, invariance_vv = tf.meshgrid(invariance_xs, invariance_vs)
@@ -101,6 +97,7 @@ class SHM_Invariance(gpflow.kernels.Kernel):
         B = tf.concat([B1, B2], 0)
         C = tf.transpose(B)
         D = tf.multiply(self.x_g_dot_squared, Ka_XgXg) + tf.multiply(self.x_g_squared, Kv_XgXg) 
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         
         return (A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D), 1), C, 1))[:2*n, 2*n:]
 
@@ -127,21 +124,22 @@ class SHM_Invariance(gpflow.kernels.Kernel):
         B = tf.multiply(K_Xg, x_g_stacked)
         C = tf.transpose(B)
         D = tf.multiply(self.x_g_dot_squared, Ka_XgXg) + tf.multiply(self.x_g_squared, Kv_XgXg)
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         
         return tf.linalg.tensor_diag_part(A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D),1), C, 1))
 
 class Pendulum_Invariance(gpflow.kernels.Kernel):
     def __init__(self, invariance_range, invar_density, jitter_size):
         super().__init__(active_dims=[0, 1])
-        self.jitter = gpflow.kernels.White(jitter_size)
-        self.RBFa = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.RBFv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
-        self.Ka =  self.RBFa + self.jitter
-        self.Kv =  self.RBFv + self.jitter
+        self.jitter = jitter_size
+        self.Ka = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+        self.Kv = gpflow.kernels.RBF(variance=1, lengthscales=[1,1]) 
+
         invariance_xs = tf.linspace(-invariance_range,invariance_range,invar_density)
         invariance_vs = tf.linspace(-invariance_range,invariance_range,invar_density)
         invariance_xx, invariance_vv = tf.meshgrid(invariance_xs, invariance_vs)
         self.invar_grids = tf.stack([tf.reshape(invariance_xx,[-1]), tf.reshape(invariance_vv,[-1])], axis=1)
+
         self.x_g_squared = tf.tensordot(tf.math.sin(self.invar_grids[:,0,None]),tf.math.sin(self.invar_grids[None,:,0]),1)
         self.x_g_dot_squared = tf.tensordot(self.invar_grids[:,1,None],self.invar_grids[None,:,1],1)
 
@@ -195,6 +193,7 @@ class Pendulum_Invariance(gpflow.kernels.Kernel):
         B = tf.concat([B1, B2], 0)
         C = tf.transpose(B)
         D = tf.multiply(self.x_g_dot_squared, Ka_XgXg) + tf.multiply(self.x_g_squared, Kv_XgXg) 
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         
         return (A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D), 1), C, 1))[:2*n, 2*n:]
 
@@ -221,6 +220,7 @@ class Pendulum_Invariance(gpflow.kernels.Kernel):
         B = tf.multiply(K_Xg, x_g_stacked)
         C = tf.transpose(B)
         D = tf.multiply(self.x_g_dot_squared, Ka_XgXg) + tf.multiply(self.x_g_squared, Kv_XgXg)
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         
         return tf.linalg.tensor_diag_part(A-tf.tensordot(tf.tensordot(B, tf.linalg.inv(D),1), C, 1))
 
@@ -238,6 +238,7 @@ class damping_pendulum_mean(gpflow.mean_functions.MeanFunction):
         self.invar_grids = kernel.invar_grids
         self.Ka = kernel.Ka
         self.Kv = kernel.Kv
+        self.jitter = kernel.jitter
         self.fixed = fixed
         self.length = length
         self.gamma = gamma
@@ -261,6 +262,7 @@ class damping_pendulum_mean(gpflow.mean_functions.MeanFunction):
         x_g_dot_squared = tf.tensordot(self.invar_grids[:,1,None],self.invar_grids[None,:,1],1)
         B = tf.multiply(K_Xg, x_g_stacked)
         D = tf.multiply(x_g_dot_squared, Ka_XgXg) + tf.multiply(x_g_squared, Kv_XgXg)
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         if self.fixed:
             return tf.tensordot(tf.tensordot(B, tf.linalg.inv(D), 1), -self.epsilon*tf.ones((self.invar_grids.shape[0], 1), dtype=tf.float64),1) 
         else:
@@ -272,6 +274,7 @@ class damping_SHM_mean(gpflow.mean_functions.MeanFunction):
         self.invar_grids = kernel.invar_grids
         self.Ka = kernel.Ka
         self.Kv = kernel.Kv
+        self.jitter = kernel.jitter
         self.gamma = gamma
         self.mass = mass
         self.fixed = fixed
@@ -295,6 +298,7 @@ class damping_SHM_mean(gpflow.mean_functions.MeanFunction):
         x_g_dot_squared = tf.tensordot(self.invar_grids[:,1,None],self.invar_grids[None,:,1],1)
         B = tf.multiply(K_Xg, x_g_stacked)
         D = tf.multiply(x_g_dot_squared, Ka_XgXg) + tf.multiply(x_g_squared, Kv_XgXg)
+        D += self.jitter*tf.eye(D.shape[0], dtype=tf.float64)
         if self.fixed:
             return tf.tensordot(tf.tensordot(B, tf.linalg.inv(D), 1), -self.epsilon*tf.ones((self.invar_grids.shape[0], 1), dtype=tf.float64),1) 
         else:
@@ -305,14 +309,14 @@ def degree_of_freedom(kernel, test_points):
     K = kernel(test_points)
     return tf.linalg.trace(tf.tensordot(K, tf.linalg.inv(K+1e-6*tf.eye(K.shape[0], dtype=tf.float64)), 1)).numpy()
 
-def get_SHM_data():
-    t = tf.linspace(0, 30, 30)
+def get_SHM_data(total_time, noise):
+    t = tf.linspace(0, total_time, total_time)
     x = tf.math.sin(t)
     v = tf.math.cos(t)
     X1 = tf.concat([x[:,None], v[:,None]], axis=-1)
     X2 = 2*X1
-    X1 += tf.random.normal((X1.shape), 0, 0.1, dtype=tf.float64)
-    X2 += tf.random.normal((X2.shape), 0, 0.1, dtype=tf.float64)
+    X1 += tf.random.normal((X1.shape), 0, noise, dtype=tf.float64)
+    X2 += tf.random.normal((X2.shape), 0, noise, dtype=tf.float64)
     Y1 = (X1[2:,:]-X1[:-2, :])/(2) 
     Y2 = (X2[2:,:]-X2[:-2, :])/(2) 
     X1 = X1[1:-1, :]
@@ -321,17 +325,17 @@ def get_SHM_data():
     Y = tf.concat([Y1, Y2], axis=0) 
     return (X, Y)
 
-def get_damped_SHM_data(gamma):
+def get_damped_SHM_data(gamma, total_time, noise):
     m = 1
     k = 1
     w = np.sqrt(k/m-gamma**2)
-    t = np.linspace(0, 30, 30)
+    t = np.linspace(0, total_time, total_time)
     x = np.sin(w*t)*np.exp(-gamma*t)
     v = np.exp(-gamma*t)*(w*np.cos(w*t)-gamma*np.sin(w*t))
     X1 = tf.concat([x[:,None], v[:,None]], axis=-1)
     X2 = 2*X1
-    X1 += tf.random.normal((X1.shape), 0, 0.1, dtype=tf.float64)
-    X2 += tf.random.normal((X2.shape), 0, 0.1, dtype=tf.float64)
+    X1 += tf.random.normal((X1.shape), 0, noise, dtype=tf.float64)
+    X2 += tf.random.normal((X2.shape), 0, noise, dtype=tf.float64)
     Y1 = (X1[2:,:]-X1[:-2, :])/(2) 
     Y2 = (X2[2:,:]-X2[:-2, :])/(2) 
     X1 = X1[1:-1, :]
@@ -340,10 +344,10 @@ def get_damped_SHM_data(gamma):
     Y = tf.concat([Y1, Y2], axis=0)
     return (X, Y)
 
-def get_pendulum_data():
+def get_pendulum_data(total_time, noise):
     dt = 0.01
-    sample = int(1/dt)
-    t = np.linspace(0, 30, int(30/dt))
+    sample_rate = int(1/dt)
+    t = np.linspace(0, total_time, int(total_time/dt))
     g = 1
     l = 1
     def f(t, r):
@@ -352,15 +356,15 @@ def get_pendulum_data():
         return np.array([omega, -g / l * np.sin(theta)])
     results = odeint(f, [np.radians(90), 0], t, tfirst=True)
     results2 = odeint(f, [np.radians(150), 0], t, tfirst=True)
-    x1 = results[0::sample,0]
-    v1 = results[0::sample,1]
-    x2 = results2[0::sample,0]
-    v2 = results2[0::sample,1]
-    t = t[0::sample]
+    x1 = results[0::sample_rate,0]
+    v1 = results[0::sample_rate,1]
+    x2 = results2[0::sample_rate,0]
+    v2 = results2[0::sample_rate,1]
+    t = t[0::sample_rate]
     X_1 = tf.concat([x1[:,None], v1[:,None]], axis=-1)
     X_2 = tf.concat([x2[:,None], v2[:,None]], axis=-1)
-    X_1 += tf.random.normal((X_1.shape), 0, 0.1, dtype=tf.float64)
-    X_2 += tf.random.normal((X_2.shape), 0, 0.1, dtype=tf.float64)
+    X_1 += tf.random.normal((X_1.shape), 0, noise, dtype=tf.float64)
+    X_2 += tf.random.normal((X_2.shape), 0, noise, dtype=tf.float64)
     Y_1 = (X_1[2:,:]-X_1[:-2, :])/(2) 
     Y_2 = (X_2[2:,:]-X_2[:-2, :])/(2) 
     X_1 = X_1[1:-1, :]
@@ -369,10 +373,10 @@ def get_pendulum_data():
     Y = tf.concat([Y_1,Y_2], axis=0)
     return (X, Y)
 
-def get_damped_pendulum_data(gamma):
+def get_damped_pendulum_data(gamma, total_time, noise):
     dt = 0.01
-    sample = int(1/dt)
-    t = np.linspace(0, 30, int(30/dt))
+    sample_rate = int(total_time/dt)
+    t = np.linspace(0, total_time, int(total_time/dt))
     g = 1
     l = 1
     def f(t, r):
@@ -381,15 +385,15 @@ def get_damped_pendulum_data(gamma):
         return np.array([omega, -g / l * np.sin(theta)-2*gamma*omega])
     results = odeint(f, [np.radians(90), 0], t, tfirst=True)
     results2 = odeint(f, [np.radians(150), 0], t, tfirst=True)
-    x1 = results[0::sample,0]
-    v1 = results[0::sample,1]
-    x2 = results2[0::sample,0]
-    v2 = results2[0::sample,1]
-    t = t[0::sample]
+    x1 = results[0::sample_rate,0]
+    v1 = results[0::sample_rate,1]
+    x2 = results2[0::sample_rate,0]
+    v2 = results2[0::sample_rate,1]
+    t = t[0::sample_rate]
     X_1 = tf.concat([x1[:,None], v1[:,None]], axis=-1)
     X_2 = tf.concat([x2[:,None], v2[:,None]], axis=-1)
-    X_1 += tf.random.normal((X_1.shape), 0, 0.1, dtype=tf.float64)
-    X_2 += tf.random.normal((X_2.shape), 0, 0.1, dtype=tf.float64)
+    X_1 += tf.random.normal((X_1.shape), 0, noise, dtype=tf.float64)
+    X_2 += tf.random.normal((X_2.shape), 0, noise, dtype=tf.float64)
     Y_1 = (X_1[2:,:]-X_1[:-2, :])/(2) 
     Y_2 = (X_2[2:,:]-X_2[:-2, :])/(2) 
     X_1 = X_1[1:-1, :]
@@ -398,18 +402,15 @@ def get_damped_pendulum_data(gamma):
     Y = tf.concat([Y_1,Y_2], axis=0)
     return (X, Y)
 
-def get_test_points():
-    test_range = 3 
-    test_density = 40
-    test_xs = tf.linspace(-test_range,test_range,test_density)
-    test_vs = tf.linspace(-test_range,test_range,test_density)
-    test_xx, test_vv = tf.meshgrid(test_xs, test_vs)
-    test_points = tf.stack([tf.reshape(test_xx,[-1]), tf.reshape(test_vv,[-1])], axis=1)
-    return test_points
+def get_grid_of_points(grid_range, grid_density):
+    grid_xs = tf.linspace(-grid_range,grid_range,grid_density)
+    grid_vs = tf.linspace(-grid_range,grid_range,grid_density)
+    grid_xx, grid_vv = tf.meshgrid(grid_xs, grid_vs)
+    grid_points = tf.stack([tf.reshape(grid_xx,[-1]), tf.reshape(grid_vv,[-1])], axis=1)
+    return grid_points
 
-def get_MOI(jitter_size):
-    moi = MOI(jitter_size)
-    set_trainable(moi.jitter.variance, False)
+def get_MOI():
+    moi = MOI()
     moi.RBFa.variance = gpflow.Parameter(moi.RBFa.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     moi.RBFv.variance = gpflow.Parameter(moi.RBFv.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     moi.RBFa.lengthscales = gpflow.Parameter(moi.RBFa.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
@@ -418,7 +419,6 @@ def get_MOI(jitter_size):
     
 def get_SHM_Invariance(invar_range, invar_density, jitter_size):
     invariance_kernel = SHM_Invariance(invar_range, invar_density, jitter_size)
-    set_trainable(invariance_kernel.jitter.variance, False)
     invariance_kernel.RBFa.variance = gpflow.Parameter(invariance_kernel.RBFa.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     invariance_kernel.RBFv.variance = gpflow.Parameter(invariance_kernel.RBFv.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     invariance_kernel.RBFa.lengthscales = gpflow.Parameter(invariance_kernel.RBFa.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
@@ -427,7 +427,6 @@ def get_SHM_Invariance(invar_range, invar_density, jitter_size):
 
 def get_Pendulum_Invariance(invar_range, invar_density, jitter_size):
     invariance_kernel = Pendulum_Invariance(invar_range, invar_density, jitter_size)
-    set_trainable(invariance_kernel.jitter.variance, False)
     invariance_kernel.RBFa.variance = gpflow.Parameter(invariance_kernel.RBFa.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     invariance_kernel.RBFv.variance = gpflow.Parameter(invariance_kernel.RBFv.variance.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
     invariance_kernel.RBFa.lengthscales = gpflow.Parameter(invariance_kernel.RBFa.lengthscales.numpy(), transform=tfp.bijectors.Sigmoid(to_default_float(0.1), to_default_float(10.))) 
@@ -441,4 +440,41 @@ def get_GPR_model(kernel, mean_function, data, test_points):
     opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(maxiter=100))
     pred, var = m.predict_f(test_points)
     return (m, pred, var)
+
+'''
+def plotting(pred, var, test_points, data, save, name, angle1, angle2, acc, lml):
+    X, Y = data
+    test_xx, test_vv = test_points
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(test_xx, test_vv, tf.reshape(pred, test_xx.shape), cmap="viridis",linewidth=0, antialiased=False, alpha=0.5)
+    surf = ax.plot_surface(test_xx, test_vv, tf.reshape(pred, test_xx.shape)+1.96*tf.math.sqrt(tf.reshape(var, test_xx.shape)), color="grey",linewidth=0, antialiased=False, alpha=0.1)
+    surf = ax.plot_surface(test_xx, test_vv, tf.reshape(pred, test_xx.shape)-1.96*tf.math.sqrt(tf.reshape(var, test_xx.shape)), color="grey",linewidth=0, antialiased=False, alpha=0.1)
+    if acc:
+        ax.scatter(X[:,0], X[:,1],Y[:,1,None], color="black", marker="o", s=3) #data
+    else:
+        ax.scatter(X[:,0], X[:,1],Y[:,0,None], color="black", marker="o", s=3) #data
+    ax.view_init(angle1,angle2)
+    ax.set_xlabel("position")
+    ax.set_ylabel("velocity")
+    if acc:
+        ax.set_zlabel("acceleration")
+    else:
+        ax.set_zlabel("velocity")
+    ax.annotate("log marginal likelihood: {0:0.2f}".format(lml), xy=(0.5, 0.9), xycoords='axes fraction')
+    if save:
+        plt.savefig(name+"_3D.pdf")
+    plt.figure(figsize=(5, 3))
+    contours = plt.contourf(test_xx, test_vv, tf.reshape(pred,(test_xx.shape)), levels=100, cmap="viridis", alpha=0.3)
+    plt.colorbar(contours)
+    if acc:
+        contours = plt.scatter(X[:,0],X[:,1],c=Y[:,1,None],cmap="viridis", alpha=0.2)
+    else:
+        contours = plt.scatter(X[:,0],X[:,1],c=Y[:,0,None],cmap="viridis", alpha=0.2)
+    plt.xlim((-test_range, test_range))
+    plt.ylim((-test_range, test_range))
+    plt.xlabel("position")
+    plt.ylabel("velocity")
+    if save:
+        plt.savefig(name+"_contour.pdf")
+'''
 
