2b3455fe95111d239a6d7951e032c60323e5250a
charlielao <43299848+charlielao@users.noreply.github.com>
2022-06-21 08:38:39 +0100

    tidy up, split to three files

--- codes/damped.py
+++ codes/damped.py
@@ -4,15 +4,17 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from scipy.integrate import solve_ivp, odeint
 from gpflow.utilities import print_summary, positive, to_default_float, set_trainable
-import invariance_functions as inv
+from invariance_mean_functions import zero_mean, damping_SHM_mean, damping_pendulum_mean
+from invariance_kernels import get_MOI, get_Pendulum_Invariance, get_SHM_Invariance
+from invariance_functions import degree_of_freedom, get_GPR_model, get_damped_SHM_data, get_damped_pendulum_data, get_grid_of_points
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 
-test_grids = inv.get_grid_of_points(3, 40)
+test_grids = get_grid_of_points(3, 40)
 print("SHM")  #switch
 for gamma in [0.05]:#[0.01, 0.05, 0.1]:
     print("current damping: %s" %gamma)
-    data = inv.get_damped_SHM_data(gamma, 30, 0.1) #switch
+    data = get_damped_SHM_data(gamma, 30, 0.1) #switch
     for fixed in [True, False]:
         if fixed:
             fixed_mean = "fixed mean"
@@ -21,12 +23,12 @@ for gamma in [0.05]:#[0.01, 0.05, 0.1]:
         print(fixed_mean)
         for jitter in [1e-6]:
             print("current jitter %s" %jitter)
-            print("Naive GP            lml: %s" %inv.get_GPR_model(inv.get_MOI(), inv.zero_mean(2), data, test_grids)[0].log_marginal_likelihood().numpy())
+            print("Naive GP            lml: %s" %get_GPR_model(get_MOI(), zero_mean(2), data, test_grids)[0].log_marginal_likelihood().numpy())
             for invar_density in [20]:#np.arange(10, 40, 10):
                     try:
-                        kernel = inv.get_SHM_Invariance(3, invar_density, jitter)#switch
-                        mean_function = inv.damping_SHM_mean(kernel, fixed, gamma, mass=1)#switch
-                        m, pred, var = inv.get_GPR_model(kernel, mean_function, data, test_grids)
+                        kernel = get_SHM_Invariance(3, invar_density, jitter)#switch
+                        mean_function = damping_SHM_mean(kernel, fixed, gamma, mass=1)#switch
+                        m, pred, var = get_GPR_model(kernel, mean_function, data, test_grids)
                         print("Invariance GP density %s lml: %s" %(invar_density, m.log_marginal_likelihood().numpy()))
 
                     except tf.errors.InvalidArgumentError:
--- codes/undamped.py
+++ codes/undamped.py
@@ -4,20 +4,22 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from scipy.integrate import solve_ivp, odeint
 from gpflow.utilities import print_summary, positive, to_default_float, set_trainable
-import invariance_functions as inv
+from invariance_mean_functions import zero_mean
+from invariance_kernels import get_MOI, get_Pendulum_Invariance, get_SHM_Invariance
+from invariance_functions import degree_of_freedom, get_GPR_model, get_SHM_data, get_pendulum_data, get_grid_of_points
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 
-test_grids = inv.get_grid_of_points(3, 40)
-zero_mean = inv.zero_mean(2)
-data = inv.get_SHM_data(30, 0.01) #switch
+test_grids = get_grid_of_points(3, 40)
+zero_mean = zero_mean(2)
+data = get_SHM_data(30, 0.01) #switch
 for jitter in [1e-5]:
     print("current jitter %s" %jitter)
-    print("Naive GP            lml: %s" %inv.get_GPR_model(inv.get_MOI(), zero_mean, data, test_grids)[0].log_marginal_likelihood().numpy())
+    print("Naive GP            lml: %s" %get_GPR_model(get_MOI(), zero_mean, data, test_grids)[0].log_marginal_likelihood().numpy())
     for invar_density in [20]: #np.arange(10, 30, 10):
             try:
-                kernel = inv.get_SHM_Invariance(3, invar_density, jitter) #switch
-                m, pred, var = inv.get_GPR_model(kernel, zero_mean, data, test_grids)
+                kernel = get_SHM_Invariance(3, invar_density, jitter) #switch
+                m, pred, var = get_GPR_model(kernel, zero_mean, data, test_grids)
                 print("Invariance GP density %s lml: %s" %(invar_density, m.log_marginal_likelihood().numpy()))
 
             except tf.errors.InvalidArgumentError:
