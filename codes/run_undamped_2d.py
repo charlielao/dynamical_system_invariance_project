3be1fe3b2177aa9105299a2cac94c4c4e5b73010
charlielao <charliezz321@gmail.com>
2022-06-29 10:54:51 +0100

    done with 2D SHM, experiemnt with random starting points

--- codes/run_undamped_2d.py
+++ codes/run_undamped_2d.py
@@ -13,16 +13,16 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '3'
 # %%
 mean = zero_mean(4)
 data = get_SHM2D_data(0.1, 2, 1e-4, [[1, 2],[0,1]], [[0,0],[0,0]]) #switch
-test_data = get_SHM2D_data(0.1, 1, 1e-4, [[2],[3]], [[1],[1]])
+test_data = get_SHM2D_data(0.1, 1, 1e-4, [[np.random.uniform(-3,3)],[np.random.uniform(-3,3)]], [[np.random.uniform(-3,3)],[np.random.uniform(-3,3)]])
 for jitter in [2e-5]:
 #    print("current jitter %s" %jitter)
-    m = get_GPR_2Dmodel(get_MOI2D(), mean, data, 100)
+    m = get_GPR_2Dmodel(get_MOI2D(), mean, data, 200)
     print("%s, "%round(m.log_marginal_likelihood().numpy()))
     evaluate_moi = evaluate_2Dmodel(m, test_data, 0.1)
     print(evaluate_moi[:2])
     for invar_density in [5]: #np.arange(10, 30, 10):
             try:
-                kernel = get_SHM2D_Local_Invariance(5, 100, jitter) #switch
+                kernel = get_SHM2D_Local_Invariance(3, 50, jitter) #switch
 #                kernel = get_SHM2D_Invariance(3, invar_density, jitter) #switch
                 m = get_GPR_2Dmodel(kernel, mean, data, 100)
                 print(round(m.log_marginal_likelihood().numpy()))
